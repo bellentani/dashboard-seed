3358fcaa3f0cad7e116fe2707e58dc6c95766705
Fernando Poe Bellentani <contato@fernandob.com.br>
2016-04-28 23:11:16 -0300

    Criação da estrutura de básica de views com HTML, CSS e um pouco de JS.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,15 @@
 module.exports = function(app) {
+  //Main page
   app.get('/', function(req, res){
     res.render('index', {
+      title: 'Express',
+      user: req.user
+    });
+  });
+
+  //Perfil do usuário
+  app.get('/profile', function(req, res){
+    res.render('profile', {
       title: 'Express',
       user: req.user
     });
---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('[data-user]').on('click', function(e) {
+      var dataTarget = $(this).data('user');
+      //console.log(dataTarget);
+      if (dataTarget == 'login') {
+        console.log('login');
+        $('#modalUserSignup').removeAttr('style');
+        $('#modalUserLogin').show();
+        $('#modalUserTitle').text('Login no site');
+      } else if (dataTarget == 'signup') {
+        $('#modalUserLogin').removeAttr('style');
+        $('#modalUserSignup').show();
+        $('#modalUserTitle').text('Cadastre-se');
+      }
+    });
+});
