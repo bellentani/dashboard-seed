7359e8bc99c78d5edb4ba6f7fda7f8ef6d34c9e8
Fernando Poe Bellentani <contato@fernandob.com.br>
2016-05-04 13:58:19 -0300

    ++

--- config/passport.js
+++ config/passport.js
@@ -1,2 +1,4 @@
+//https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+
 var LocalStrategy   = require('passport-local').Strategy;
 var User            = require('../app/models/user');
