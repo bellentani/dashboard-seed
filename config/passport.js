9a90d5118334be2bef22029444aea76795fd1674
Fernando Poe Bellentani <contato@fernandob.com.br>
2016-05-05 00:17:27 -0300

    Resolvendo conflitos.

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,57 @@
 //https://scotch.io/tutorials/easy-node-authentication-setup-and-local
 
-var LocalStrategy   = require('passport-local').Strategy;
-var User            = require('../app/models/user');
+var LocalStrategy  = require('passport-local').Strategy;
+var User = require('../app/models/user');
+
+module.exports = function(passport) {
+  passport.serializeUser(function(user, done) {
+      done(null, user.id);
+  });
+  passport.deserializeUser(function(id, done) {
+      User.findById(id, function(err, user) {
+          done(err, user);
+      });
+  });
+  //signup
+  passport.use('local-signup', new LocalStrategy({
+      // by default, local strategy uses username and password, we will override with email
+      usernameField : 'email',
+      passwordField : 'password',
+      passReqToCallback : true // allows us to pass back the entire request to the callback
+  },
+  function(req, email, password, done) {
+
+      // asynchronous
+      // User.findOne wont fire unless data is sent back
+      process.nextTick(function() {
+
+      User.findOne({ 'local.email' :  email }, function(err, user) {
+        // if there are any errors, return the error
+        if (err)
+            return done(err);
+
+        // check to see if theres already a user with that email
+        if (user) {
+            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+        } else {
+
+          // if there is no user with that email
+          // create the user
+          var newUser            = new User();
+
+          // set the user's local credentials
+          newUser.local.email    = email;
+          newUser.local.email    = email;
+          newUser.local.password = newUser.generateHash(password);
+
+          // save the user
+          newUser.save(function(err) {
+              if (err)
+                  throw err;
+              return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+};
