56cf56f5251633a407fa0518225da7a5978df15b
Fernando Poe Bellentani <contato@fernandob.com.br>
2016-05-15 11:23:26 -0300

    Layout da página principal com menu depois de logado.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-	//vars gerais
+	//Exibe o conteúdo específico de cada modal basedo no clique do botão
 	$('[data-user]').on('click', function(e) {
 		var dataTarget = $(this).data('user');
 		console.log(dataTarget);
@@ -12,5 +12,31 @@ $(function() {
 			$('#modalUserSignup').show();
 			$('#modalUserTitle').text('Cadastre-se');
 		}
+	});
+
+	//Abre submenus
+	$('.submenu-opener').on('click', function(event) {
+		event.preventDefault();
+		var submenu = $(this).siblings('.submenu');
+		if (!submenu.hasClass('in')) {
+			submenu.addClass('in');
+			submenu.slideToggle(function() {
+				$(this).removeClass('in');
+				if ($(this).is(':visible')) {
+					$(this).addClass('opened');
+				} else {
+					$(this).removeClass('opened');
+				}
+			});
+		}
+	});
+
+	//Fecha as coisas se clica fora
+	$(document).click(function(event) {
+    if(!$(event.target).closest('.submenu').length && !$(event.target).is('.submenu, .submenu-opener')) {
+      if($('.submenu').is(":visible")) {
+          $('.submenu').slideUp();
+      }
+    }
 	});
 });
